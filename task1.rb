8342efc1b0b15bdefafbec485bb6fa59d025dec4
Adamenko <mangamen45@gmail.com>
2017-02-19 20:59:18 +0200

    first task complete

---
+++ task1.rb
@@ -0,0 +1,13 @@
+# adittion class string
+class String
+
+  def valid?
+    stack = []
+    skobki = { '{' => '}', '[' => ']', '(' => ')', '<' => '>' }
+    self.each_char do |char|
+      stack << char if skobki.key?(char)
+      return false if skobki.key(char) && skobki.key(char) != stack.pop
+    end
+    stack.empty?
+  end
+end
